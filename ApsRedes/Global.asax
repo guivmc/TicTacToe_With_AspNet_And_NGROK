5a5dfba5ee1b5a394c263d1e3f8e40e1f28684e5
guivmc <guivmc@hotmail.com>
2018-10-08 10:32:40 -0300

    Update0.1

---
+++ ApsRedes/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApsRedes.MvcApplication" Language="C#" %>
---
+++ ApsRedes/Models/Player.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApsRedes.Models
+{
+    public class Player
+    {
+        [Required]
+        [StringLength(5)]
+        [DisplayName("Username")]
+        public string name { get; set; }
+
+        public char mark { get; set; }
+    }
+}
